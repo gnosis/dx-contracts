ad2049911e093e8d179bd8e250600379b430fa56
Velenir <Velenir@users.noreply.github.com>
2017-12-21 13:48:09 +0300

    made withdraw script work with the latest DutchExchange contract

--- trufflescripts/withdraw.js
+++ trufflescripts/withdraw.js
@@ -1,12 +1,10 @@
-const DutchExchange = artifacts.require('./DutchExchange.sol')
-const TokenETH = artifacts.require('./EtherToken.sol')
-const TokenGNO = artifacts.require('./TokenGNO.sol')
+const { getTokenDeposits, withrawFromDX } = require('./utils/contracts')(artifacts)
 
 const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
 
 /**
  * truffle exec trufflescripts/withdraw.js
- * to withdraw funds to DutchExchange contracts
+ * to withdraw funds from DutchExchange contract
  * @flags:
  * --seller                     as the seller
  * --buyer                      as the buyer
@@ -21,10 +19,6 @@ module.exports = async () => {
     return
   }
 
-  const dx = await DutchExchange.deployed()
-  const eth = await TokenETH.deployed()
-  const gno = await TokenGNO.deployed()
-
   let account, accountName
   if (argv.a) account = accountName = argv.a
   else if (argv.seller) {
@@ -35,33 +29,15 @@ module.exports = async () => {
     accountName = 'Buyer'
   }
 
-  const getBalances = acc => Promise.all([
-    dx.balances(eth.address, acc),
-    dx.balances(gno.address, acc),
-  ]).then(res => res.map(n => n.toNumber()))
-
   console.log(`${accountName}`)
 
-  let [accountETH, accountGNO] = await getBalances(account)
-  console.log(`Deposit was:\t${accountETH}\tETH,\t${accountGNO}\tGNO`)
+  let { ETH, GNO } = await getTokenDeposits(account)
+  console.log(`Deposit was:\t${ETH}\tETH,\t${GNO}\tGNO`)
 
+  const tokensToWithdraw = { ETH: argv.eth, GNO: argv.gno, TUL: argv.tul, OWL: argv.owl }
 
-  if (argv.eth) {
-    try {
-      await dx.withdraw(eth.address, argv.eth, { from: account })
-    } catch (error) {
-      console.warn(error.message || error)
-    }
-  }
+  await withrawFromDX(account, tokensToWithdraw);
 
-  if (argv.eth) {
-    try {
-      await dx.withdraw(eth.address, argv.eth, { from: account })
-    } catch (error) {
-      console.warn(error.message || error)
-    }
-  }
-
-  [accountETH, accountGNO] = await getBalances(account)
-  console.log(`Deposit is:\t${accountETH}\tETH,\t${accountGNO}\tGNO`)
+  ({ ETH, GNO } = await getTokenDeposits(account))
+  console.log(`Deposit is:\t${ETH}\tETH,\t${GNO}\tGNO`)
 }
