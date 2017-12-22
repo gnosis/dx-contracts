56d6e51a72f1b57310cd12c28ccac478f654b023
David Sato <david.sato64@gmail.com>
2017-12-22 15:45:57 +0100

    added eventWatcher and logger fns

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,5 @@
-async function assertRejects(q, msg) {
+/* eslint no-console:0, no-confusing-arrow:0 */
+const assertRejects = async (q, msg) => {
   let res, catchFlag = false
   try {
     res = await q
@@ -15,4 +16,19 @@ const blockNumber = () => web3.eth.blockNumber
 
 const timestamp = (block = 'latest') => web3.eth.getBlock(block).timestamp
 
-module.exports = { assertRejects, timestamp, blockNumber }
+const logger = async (desc, fn) => console.log(`---- \n => ${desc} ${fn ? `|| - - - - - - - - - -  - > ${fn}` : ''}`)
+/**
+ * eventWatcher                - ...watches events
+ * @param {contract} contract  - dx, usually
+ * @param {string} event       - name of event on DutchExchange.sol to track
+ * @param {Object} args?       - not required, args to look for
+ */
+const eventWatcher = (contract, event, args) => contract[event](args).watch((err, result) => err ? console.log(err) : console.log('Found', result))
+
+module.exports = {
+  assertRejects,
+  timestamp,
+  blockNumber,
+  logger,
+  eventWatcher,
+}
