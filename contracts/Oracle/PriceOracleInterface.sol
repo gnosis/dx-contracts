f3a4bba225917e1cdca0fb4356cf40a4040c4365
David Sato <david.sato64@gmail.com>
2018-01-05 14:59:46 +0100

    changed weird pathing format in contracts + linted across files

--- contracts/Oracle/PriceOracleInterface.sol
+++ contracts/Oracle/PriceOracleInterface.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
-import "./../DutchExchange/DutchExchangeInterface.sol";
-import "./../Utils/Math.sol";
+import "../DutchExchange/DutchExchangeInterface.sol";
+import "../Utils/Math.sol";
 
 contract PriceOracleInterface {
     using Math for *;
--- contracts/Tokens/TokenTUL.sol
+++ contracts/Tokens/TokenTUL.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.4.18;
-import "../Tokens/StandardToken.sol";
+import "./StandardToken.sol";
 import "../Utils/Math.sol";
 
 
