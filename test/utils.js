c1268be2a5fcd1aceac9167257f2dcb19efa5f68
Velenir <Velenir@users.noreply.github.com>
2018-01-08 18:50:22 +0300

    implemented stopWatching function on eventWatcher utility function

--- test/utils.js
+++ test/utils.js
@@ -17,13 +17,43 @@ const blockNumber = () => web3.eth.blockNumber
 const timestamp = (block = 'latest') => web3.eth.getBlock(block).timestamp
 
 const logger = async (desc, fn) => console.log(`---- \n => ${desc} ${fn ? `|| - - - - - - - - - -  - > ${fn}` : ''}`)
+
+// keeps track of watched events
+const stopWatching = {}
 /**
  * eventWatcher                - ...watches events
  * @param {contract} contract  - dx, usually
  * @param {string} event       - name of event on DutchExchange.sol to track
  * @param {Object} args?       - not required, args to look for
+ * @returns stopWatching function
  */
-const eventWatcher = (contract, event, args) => contract[event](args).watch((err, result) => err ? console.log(err) : console.log('Found', result))
+const eventWatcher = (contract, event, args) => {
+  const eventObject = contract[event](args).watch((err, result) => err ? console.log(err) : console.log('Found', result))
+  const unwatch = stopWatching[event] = eventObject.stopWatching.bind(eventObject)
+
+  return unwatch
+}
+
+eventWatcher.stopWatching = (event) => {
+  // if given particular event name, stop watching it
+  if (event && typeof event === 'string') {
+    const unwatch = stopWatching[event]
+    if (unwatch) {
+      unwatch()
+      delete stopWatching[event]
+    } else {
+      console.log(`${event} event was never watched`)
+    }
+
+    return
+  }
+
+  // otherwise stop watching all events
+  for (const key of Object.keys(stopWatching)) {
+    stopWatching[key]()
+    delete stopWatching[key]
+  }
+}
 
 module.exports = {
   assertRejects,
