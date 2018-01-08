ccba323454d620719e67de3f0bc3716177bcf3ca
David Sato <david.sato64@gmail.com>
2018-01-08 19:04:24 +0100

    adding tulip tests

--- test/testFunctions.js
+++ test/testFunctions.js
@@ -68,7 +68,13 @@ const setAndCheckAuctionStarted = async (ST, BT) => {
 
   // wait for the right time to send buyOrder
 
-  await wait(startingTimeOfAuction - timestamp())
+  await wait((startingTimeOfAuction - timestamp()) + 500)
+
+  console.log(`
+  time now ----------> ${new Date(timestamp() * 1000)}
+  auction starts ----> ${new Date(startingTimeOfAuction * 1000)}
+  `)
+
   assert.equal(timestamp() >= startingTimeOfAuction, true)
 }
 
@@ -129,45 +135,41 @@ const getAuctionIndex = async (sell, buy) => {
 
   return (await dx.getAuctionIndex.call(buy.address, sell.address)).toNumber()
 }
-// const getStartingTimeOfAuction = async (sell = eth, buy = gno) => (await dx.getAuctionStart.call(sell.address, buy.address)).toNumber()
+const getStartingTimeOfAuction = async (sell = eth, buy = gno) => (await dx.getAuctionStart.call(sell.address, buy.address)).toNumber()
 
-module.exports = {
-  setupTest,
-  getContracts,
-  getAuctionIndex,
-  checkBalanceBeforeClaim,
-  waitUntilPriceIsXPercentOfPreviousPrice,
-  setAndCheckAuctionStarted,
+/**
+ * address sellToken,
+ * address buyToken,
+ * uint auctionIndex,
+ * uint amount
+ */
+const postBuyOrder = async (ST, BT, aucIdx, amt, acct) => {
+  const { DutchExchange: dx, EtherToken: eth, TokenGNO: gno } = await getContracts()
+  ST = ST || eth; BT = BT || gno
+  let auctionIdx = aucIdx || await getAuctionIndex()
+
+  console.log(`
+  Posting Buy Amt -------> ${amt} in ETH for GNO
+  `)
+
+  return dx.postBuyOrder(ST.address, BT.address, auctionIdx, amt, { from: acct })
 }
 
-// const setupTest2 = async (accounts) => {
-//   // get buyers, sellers set up and running
-//   gno = await TokenGNO.deployed()
-//   eth = await EtherToken.deployed()
-//   tokenTUL = await TokenTUL.deployed()
-//   // create dx
-//   dx = await DutchExchange.deployed()
-//   // create price Oracle
-//   oracle = await PriceOracle.deployed()
+const claimBuyerFunds = async (ST, BT, user, aucIdx, acct) => {
+  const { DutchExchange: dx, EtherToken: eth, TokenGNO: gno } = await getContracts()
+  ST = ST || eth; BT = BT || gno; user = user || acct
+  let auctionIdx = aucIdx || await getAuctionIndex()
 
-//   // Await ALL Promises for each account setup
-//   await Promise.all(accounts.map((acct) => {
-//     /* eslint array-callback-return:0 */
-//     if (acct === accounts[0]) return
+  return dx.claimBuyerFunds(ST.address, BT.address, user, auctionIdx, { from: acct })
+}
 
-//     eth.deposit({ from: acct, value: 10 ** 9 })
-//     eth.approve(dx.address, 10 ** 9, { from: acct })
-//     gno.transfer(acct, 10 ** 18, { from: accounts[0] })
-//     gno.approve(dx.address, 10 ** 18, { from: acct })
-//   }))
-//   // Deposit depends on ABOVE finishing first... so run here
-//   await Promise.all(accounts.map((acct) => {
-//     if (acct === accounts[0]) return
-
-//     dx.deposit(eth.address, 10 ** 9, { from: acct })
-//     dx.deposit(gno.address, 10 ** 18, { from: acct })
-//   }))
-//   // add token Pair
-//   // updating the oracle Price. Needs to be changed later to another mechanism
-//   await oracle.updateETHUSDPrice(60000, { from: accounts[0] })
-// }
+module.exports = {
+  checkBalanceBeforeClaim,
+  claimBuyerFunds,
+  getAuctionIndex,
+  getContracts,
+  postBuyOrder,
+  setAndCheckAuctionStarted,
+  setupTest,
+  waitUntilPriceIsXPercentOfPreviousPrice,
+}
