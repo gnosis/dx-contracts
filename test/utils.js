5bdfef60a120799dce45d3e85ba615d8879eb21a
Velenir <Velenir@users.noreply.github.com>
2018-01-10 12:40:41 +0300

    added check for "R<number>" substring

--- test/utils.js
+++ test/utils.js
@@ -3,7 +3,8 @@ const assertRejects = async (q, msg) => {
   let res, catchFlag = false
   try {
     res = await q
-    catchFlag = res.logs && !!res.logs.find(log => log.event === 'Log')
+    // checks if there was a Log event and its argument l contains string "R<number>"
+    catchFlag = res.logs && !!res.logs.find(log => log.event === 'Log' && /\bR(\d+\.?)+/.test(log.args.l))
   } catch (e) {
     catchFlag = true
   } finally {
