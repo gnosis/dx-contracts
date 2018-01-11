3cfb8210b27729a4e3be130deb026dfadb93a11d
Velenir <Velenir@users.noreply.github.com>
2018-01-11 12:51:43 +0300

    added withdraw whole deposit test

--- test/dutchExchange.js
+++ test/dutchExchange.js
@@ -251,7 +251,7 @@ contract('DutchExchange', (accounts) => {
 })
 
 contract('DutchExchange', (accounts) => {
-  const [master, seller1, seller2, buyer1, buyer2] = accounts
+  const [master] = accounts
   const testingAccs = accounts.slice(1, 5)
 
   const ETHBalance = 10 ** 9
@@ -302,10 +302,10 @@ contract('DutchExchange', (accounts) => {
   }
 
   it('intially deposits are 0', () => Promise.all(testingAccs.map(async (acc) => {
-      const { ETH, GNO } = await getAccDeposits(acc)
+    const { ETH, GNO } = await getAccDeposits(acc)
 
-      assert.strictEqual(ETH, 0, `${acc} ETH deposit should be 0`)
-      assert.strictEqual(GNO, 0, `${acc} GNO deposit should be 0`)
+    assert.strictEqual(ETH, 0, `${acc} ETH deposit should be 0`)
+    assert.strictEqual(GNO, 0, `${acc} GNO deposit should be 0`)
   })))
 
   it('can deposit the right amount ', () => Promise.all(testingAccs.map(async (acc) => {
@@ -335,72 +335,66 @@ contract('DutchExchange', (accounts) => {
   })))
 
   it('can withdraw the right amount ', () => Promise.all(testingAccs.map(async (acc) => {
-      const withdrawETH = 90
-      const withdrawGNO = 150
+    const withdrawETH = 90
+    const withdrawGNO = 150
 
-      const { ETH, GNO } = await getAccDeposits(acc)
+    const { ETH, GNO } = await getAccDeposits(acc)
 
-      // make sure we don't withdraw more than available
-      assert.isBelow(withdrawETH, ETH, 'trying to withdraw more ETH than available')
+    // make sure we don't withdraw more than available
+    assert.isBelow(withdrawETH, ETH, 'trying to withdraw more ETH than available')
     assert.isBelow(withdrawGNO, GNO, 'trying to withdraw more GNO than available')
 
-      const { ETH: ETHDep1, GNO: GNODep1 } = await getAccDeposits(acc)
-      const { ETH: ETHBal1, GNO: GNOBal1 } = await getAccBalances(acc)
+    const { ETH: ETHDep1, GNO: GNODep1 } = await getAccDeposits(acc)
+    const { ETH: ETHBal1, GNO: GNOBal1 } = await getAccBalances(acc)
 
-      logger(`${acc} withdrawing\t${withdrawETH} ETH,\t${withdrawGNO} GNO`)
+    logger(`${acc} withdrawing\t${withdrawETH} ETH,\t${withdrawGNO} GNO`)
 
-      await dx.withdraw(eth.address, withdrawETH, { from: acc })
-      await dx.withdraw(gno.address, withdrawGNO, { from: acc })
+    await dx.withdraw(eth.address, withdrawETH, { from: acc })
+    await dx.withdraw(gno.address, withdrawGNO, { from: acc })
 
-      const { ETH: ETHDep2, GNO: GNODep2 } = await getAccDeposits(acc)
-      const { ETH: ETHBal2, GNO: GNOBal2 } = await getAccBalances(acc)
+    const { ETH: ETHDep2, GNO: GNODep2 } = await getAccDeposits(acc)
+    const { ETH: ETHBal2, GNO: GNOBal2 } = await getAccBalances(acc)
 
-      logger(`${acc} deposits:\t${ETHDep2} ETH,\t${GNODep2} GNO`)
-      assert.strictEqual(ETHDep1 - ETHDep2, withdrawETH, 'ETH deposit should decrease by the amount withdrawn')
-      assert.strictEqual(GNODep1 - GNODep2, withdrawGNO, 'GNO deposit should decrease by the amount withdrawn')
+    logger(`${acc} deposits:\t${ETHDep2} ETH,\t${GNODep2} GNO`)
+    assert.strictEqual(ETHDep1 - ETHDep2, withdrawETH, 'ETH deposit should decrease by the amount withdrawn')
+    assert.strictEqual(GNODep1 - GNODep2, withdrawGNO, 'GNO deposit should decrease by the amount withdrawn')
 
-      assert.strictEqual(ETHBal2 - ETHBal1, withdrawETH, 'ETH balance should increase by the amount withdrawn')
-      assert.strictEqual(GNOBal2 - GNOBal1, withdrawGNO, 'GNO balance should increase by the amount withdrawn')
+    assert.strictEqual(ETHBal2 - ETHBal1, withdrawETH, 'ETH balance should increase by the amount withdrawn')
+    assert.strictEqual(GNOBal2 - GNOBal1, withdrawGNO, 'GNO balance should increase by the amount withdrawn')
   })))
 
-  it.skip('test a trade on the opposite pair', async () => {
-    let auctionIndex
+  it('withdraws the whole deposit when trying to withdraw more than available', () => Promise.all(testingAccs.map(async (acc) => {
+    const withdrawETH = 290
+    const withdrawGNO = 350
 
-    // ASSERT Auction has started
-    await setAndCheckAuctionStarted(eth, gno)
-    auctionIndex = await getAuctionIndex()
-    await waitUntilPriceIsXPercentOfPreviousPrice(eth, gno, 1)
-    await dx.postBuyOrder(eth.address, gno.address, auctionIndex, 10 ** 9 * 2, { from: buyer1 })
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    console.log('this one isnt working right?')
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    await dx.postBuyOrder(gno.address, eth.address, auctionIndex, 10 ** 7 * 25, { from: seller2 })
-    logger('startingReclaiming')
-    logger('dx.getPrice.num eth gno', await dx.testing2.call(eth.address, gno.address, auctionIndex))
-    logger('dx.getPrice.num gno/eth', await dx.testing2.call(gno.address, eth.address, auctionIndex))
-    logger('dx.oralcePrice.num gno/eth ', await dx.testing.call(gno.address))
-    logger('dx.oralcePrice.num gno/eth ', await dx.testing.call(eth.address))
-    // claim buyer1 BUYER funds
-    await checkBalanceBeforeClaim(buyer1, auctionIndex, 'buyer', eth, gno, (10 ** 9 - 10 ** 9 / 200))
-    // claim seller2 BUYER funds - RECIPROCAL
-    await checkBalanceBeforeClaim(seller2, auctionIndex, 'buyer', gno, eth, (10 ** 8 * 5 - 10 ** 8 * 5 / 200))
-    // claim SELLER funds
-    await checkBalanceBeforeClaim(seller1, auctionIndex, 'seller', eth, gno, (10 ** 9 * 2 - 10 ** 9 * 2 / 200))
+    const { ETH: ETHDep1, GNO: GNODep1 } = await getAccDeposits(acc)
 
-    // post new sell order to start next auction
-    // startingTimeOfAuction = await getStartingTimeOfAuction(eth, gno)
-    auctionIndex = await getAuctionIndex()
-    logger('new auction index:', auctionIndex)
-    logger('auctionStartDate', (await dx.getAuctionStart(eth.address, gno.address)).toNumber())
+    // make sure we try to withdraw more than available
+    assert.isAbove(withdrawETH, ETHDep1, 'should try to withdraw more ETH than available')
+    assert.isAbove(withdrawGNO, GNODep1, 'should try to withdraw more GNO than available')
 
-    await dx.postSellOrder(eth.address, gno.address, auctionIndex, 10 ** 9, { from: seller2 })
+    const { ETH: ETHBal1, GNO: GNOBal1 } = await getAccBalances(acc)
 
-    // check Auction has started
-    await setAndCheckAuctionStarted(eth, gno)
+    logger(`${acc} trying to withdraw\t${withdrawETH} ETH,\t${withdrawGNO} GNO`)
 
-    auctionIndex = await getAuctionIndex()
-    await dx.postBuyOrder(eth.address, gno.address, auctionIndex, 10 ** 9 * 2, { from: buyer2 })
-  })
+    // console.log(JSON.stringify(await dx.withdraw(eth.address, withdrawETH, { from: acc }), null, 2))
+    // DutchExchange::withdraw Math.min resulted in balances[tokenAddress][msg.sender]
+    await dx.withdraw(eth.address, withdrawETH, { from: acc })
+    await dx.withdraw(gno.address, withdrawGNO, { from: acc })
+    // assert.throws(() => dx.withdraw(eth.address, withdrawETH, { from: acc }))
+
+    const { ETH: ETHDep2, GNO: GNODep2 } = await getAccDeposits(acc)
+    const { ETH: ETHBal2, GNO: GNOBal2 } = await getAccBalances(acc)
+
+    logger(`${acc} deposits:\t${ETHDep2} ETH,\t${GNODep2} GNO`)
+    // all deposits were withdrawn
+    assert.strictEqual(ETHDep2, 0, 'ETH deposit should be 0')
+    assert.strictEqual(GNODep2, 0, 'GNO deposit should be 0')
+
+    // balance increased by the actual amount withdrawn, not how uch we tried to withdraw
+    assert.strictEqual(ETHBal2 - ETHBal1, ETHDep1, 'ETH balance should increase by the amount withdrawn (whole deposit)')
+    assert.strictEqual(GNOBal2 - GNOBal1, GNODep1, 'GNO balance should increase by the amount withdrawn (whole deposit)')
+  })))
 })
 
 
