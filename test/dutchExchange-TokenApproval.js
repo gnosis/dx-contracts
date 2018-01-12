c3ca1144548efd9863eb6720361452869ba59c78
Velenir <Velenir@users.noreply.github.com>
2018-01-12 13:49:17 +0300

    added changing token approval tests

--- test/dutchExchange-TokenApproval.js
+++ test/dutchExchange-TokenApproval.js
@@ -40,8 +40,26 @@ contract('DutchExchange updating token aprroval', (accounts) => {
     const symbol = await token.symbol.call()
     const approved = await getTokenApproval(token)
 
-    logger(`Token ${symbol} at ${token.address} is ${approved ? '' : 'NOT'} approved`)
+    logger(`Token ${symbol} at ${token.address} is ${approved ? '' : 'NOT'} APPROVED`)
 
-    assert.isFalse(approved, 'token shouldn\'t be approved yet')
+    assert.isFalse(approved, `${symbol} token shouldn't be approved yet`)
+  })))
+
+  it('owner can change token approval', () => Promise.all(testingTokens.map(async (token) => {
+    const symbol = await token.symbol.call()
+    const approved1 = await getTokenApproval(token)
+
+    const owner = await dx.owner.call()
+    assert.strictEqual(owner, master, 'web3.eth.accounts[0] should be DutchExchange contract owner')
+
+    logger(`Token ${symbol} at ${token.address} is ${approved1 ? '' : 'NOT'} APPROVED`)
+    logger(`Changing ${symbol} approval to ${!approved1}`)
+
+    dx.updateApprovalOfToken(token.address, !approved1, { from: master })
+
+    const approved2 = await getTokenApproval(token)
+    logger(`Token ${symbol} at ${token.address} is ${approved2 ? '' : 'NOT'} APPROVED`)
+
+    assert.strictEqual(!approved1, approved2, ` ${symbol} token should change approval`)
   })))
 })
