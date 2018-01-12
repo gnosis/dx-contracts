4769a1d7d0101da6208814923a662390cc47a501
Velenir <Velenir@users.noreply.github.com>
2018-01-12 14:01:07 +0300

    added test for not owner setting approval

--- test/dutchExchange-TokenApproval.js
+++ test/dutchExchange-TokenApproval.js
@@ -14,7 +14,7 @@ let dx
 let contracts
 
 contract('DutchExchange updating token aprroval', (accounts) => {
-  const [master] = accounts
+  const [master, seller1] = accounts
   let testingTokens
 
   before(async () => {
@@ -53,13 +53,31 @@ contract('DutchExchange updating token aprroval', (accounts) => {
     assert.strictEqual(owner, master, 'web3.eth.accounts[0] should be DutchExchange contract owner')
 
     logger(`Token ${symbol} at ${token.address} is ${approved1 ? '' : 'NOT'} APPROVED`)
-    logger(`Changing ${symbol} approval to ${!approved1}`)
+    logger(`Owner changes ${symbol} approval to ${!approved1}`)
 
-    dx.updateApprovalOfToken(token.address, !approved1, { from: master })
+    await dx.updateApprovalOfToken(token.address, !approved1, { from: master })
 
     const approved2 = await getTokenApproval(token)
     logger(`Token ${symbol} at ${token.address} is ${approved2 ? '' : 'NOT'} APPROVED`)
 
     assert.strictEqual(!approved1, approved2, ` ${symbol} token should change approval`)
+  })))
+
+  it('not owner can\'t change token approval', () => Promise.all(testingTokens.map(async (token) => {
+    const symbol = await token.symbol.call()
+    const approved1 = await getTokenApproval(token)
+
+    const owner = await dx.owner.call()
+    assert.notStrictEqual(owner, seller1, 'web3.eth.accounts[1] should not be DutchExchange contract owner')
+
+    logger(`Token ${symbol} at ${token.address} is ${approved1 ? '' : 'NOT'} APPROVED`)
+    logger(`Not owner tries to change ${symbol} approval to ${!approved1}`)
+
+    await assertRejects(dx.updateApprovalOfToken(token.address, !approved1, { from: seller1 }), `not owner can't change ${symbol} token approval`)
+
+    const approved2 = await getTokenApproval(token)
+    logger(`Token ${symbol} at ${token.address} is ${approved2 ? '' : 'NOT'} APPROVED`)
+
+    assert.strictEqual(approved1, approved2, ` ${symbol} token should not change approval`)
   })))
 })
