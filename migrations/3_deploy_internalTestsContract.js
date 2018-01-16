ed0a154f247d6e3cabc6c1597f05f2ccb7c482ef
Velenir <Velenir@users.noreply.github.com>
2018-01-16 17:59:50 +0300

    InternalTests contract deploys (thanks to Stefan)

---
+++ migrations/3_deploy_internalTestsContract.js
@@ -0,0 +1,27 @@
+/* eslint no-multi-spaces: 0, no-console: 0 */
+
+const Math = artifacts.require('Math')
+const DutchExchange = artifacts.require('DutchExchange')
+const InternalTests = artifacts.require('InternalTests')
+
+module.exports = function deploy(deployer) {
+  deployer.link(Math, InternalTests)
+    .then(() => DutchExchange.deployed())
+    .then((dx) => {
+      const initParams = Promise.all([
+        dx.TUL.call(),
+        dx.OWL.call(),
+        dx.owner.call(),
+        dx.ETH.call(),
+        dx.ETHUSDOracle.call(),
+        dx.thresholdNewTokenPair.call(),
+        dx.thresholdNewAuction.call(),
+      ])
+
+
+      return initParams
+    }).then((initParams) => {
+      console.log(`deploying InternalFunctions test contract with ${initParams}`)
+      return  deployer.deploy(InternalTests, ...initParams)
+    })
+}
