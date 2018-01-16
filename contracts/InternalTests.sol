97a19fcfa63c71e8715e38ac856dc8c75cbadff8
Velenir <Velenir@users.noreply.github.com>
2018-01-16 16:46:30 +0300

    added deployment of contract inherited from DutchExchange (breaks deployment)

---
+++ contracts/InternalTests.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+import "./DutchExchange.sol";
+
+contract InternalTests is DutchExchange {
+  // function settleFee1(
+  //   address primaryToken,
+  //   address secondaryToken,
+  //   uint auctionIndex,
+  //   address user,
+  //   uint amount
+  // )
+  //   public
+  //   returns (uint amountAfterFee)
+  // {
+  //   return super.settleFee(primaryToken, secondaryToken, auctionIndex, user, amount);
+  // }
+}
---
+++ migrations/deploy_internalTestsContract.js
@@ -0,0 +1,64 @@
+/* eslint no-multi-spaces: 0, no-console: 0 */
+
+// const Math = artifacts.require('Math')
+const DutchExchange = artifacts.require('DutchExchange')
+const InternalTests = artifacts.require('InternalTests')
+
+// const EtherToken = artifacts.require('EtherToken')
+// const PriceOracle = artifacts.require('PriceOracle')
+// const StandardToken = artifacts.require('StandardToken')
+// const TokenGNO = artifacts.require('TokenGNO')
+// const TokenOWL = artifacts.require('TokenOWL')
+// const TokenTUL = artifacts.require('TokenTUL')
+
+module.exports = function deploy(deployer) {
+  deployer.deploy(InternalTests, { gas: 4612388 })
+  // DutchExchange.deployed().then((dx) => {
+  //   const initParams = Promise.all([
+  //     dx.TUL.call(),
+  //     dx.OWL.call(),
+  //     dx.owner.call(),
+  //     dx.ETH.call(),
+  //     dx.ETHUSDOracle.call(),
+  //     dx.thresholdNewTokenPair.call(),
+  //     dx.thresholdNewAuction.call(),
+  //   ])
+
+  
+  //   return initParams
+  // }).then((initParams) => {
+  //   console.log(`deploying InternalFunctionstest contract with ${initParams}`)
+  //   return  deployer.deploy(InternalTests, ...initParams, { gas: 4612388 })
+  // })
+//   let PriceOracleInstance
+//   // let TULInstance;
+//   deployer.deploy(Math)
+//   // StandardToken is NECESSARRY to deploy here as it is LINKED w/Math
+//   deployer.link(Math, [PriceOracle, DutchExchange, StandardToken, EtherToken, TokenGNO, TokenTUL, TokenOWL])
+
+//   deployer.deploy(EtherToken)
+//     .then(() => deployer.deploy(TokenGNO, 10 ** 19))
+//     .then(() => deployer.deploy(TokenTUL, accounts[0], accounts[0]))
+//     // StandardToken is NECESSARRY to deploy here as it is LINKED w/Math
+//     .then(() => deployer.deploy(StandardToken))
+//     .then(() => deployer.deploy(PriceOracle, accounts[0], EtherToken.address))
+//     .then(() => PriceOracle.deployed())
+//     .then((p) => {
+//       PriceOracleInstance = p
+//       return deployer.deploy(TokenOWL, TokenGNO.address /* ,PriceOracle.adress */, 0)
+//     })
+//     // @dev DX Constructor creates exchange
+//     .then(() => deployer.deploy(
+//       DutchExchange,              // Contract Name
+//       TokenTUL.address,
+//       TokenOWL.address,
+//       accounts[0],                // @param _owner will be the admin of the contract
+//       EtherToken.address,         // @param _ETH                - address of ETH ERC-20 token
+//       PriceOracle.address,        // @param _priceOracleAddress - address of priceOracle
+//       10000,
+//       1000,
+//     ))
+//     .then(() => PriceOracleInstance.updateDutchExchange(DutchExchange.address, { from: accounts[0] }))
+//     .then(() => TokenTUL.deployed())
+//     .then(T => T.updateMinter(DutchExchange.address))
+}
