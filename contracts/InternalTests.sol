971f7e39070f4964c34286e8087cb8a2582fcedf
Velenir <Velenir@users.noreply.github.com>
2018-01-16 17:59:50 +0300

    InternalTests contract deploys (thanks to Stefan)

--- contracts/InternalTests.sol
+++ contracts/InternalTests.sol
@@ -3,16 +3,36 @@ pragma solidity ^0.4.18;
 import "./DutchExchange.sol";
 
 contract InternalTests is DutchExchange {
-  // function settleFee1(
-  //   address primaryToken,
-  //   address secondaryToken,
-  //   uint auctionIndex,
-  //   address user,
-  //   uint amount
-  // )
-  //   public
-  //   returns (uint amountAfterFee)
-  // {
-  //   return super.settleFee(primaryToken, secondaryToken, auctionIndex, user, amount);
-  // }
+  function settleFeePub(
+    address primaryToken,
+    address secondaryToken,
+    uint auctionIndex,
+    address user,
+    uint amount
+  )
+    public
+    returns (uint)
+  {
+    return super.settleFee(primaryToken, secondaryToken, auctionIndex, user, amount);
+  }
+  
+  function InternalTests(
+        address _TUL,
+        address _OWL,
+        address _owner, 
+        address _ETH,
+        address _ETHUSDOracle,
+        uint _thresholdNewTokenPair,
+        uint _thresholdNewAuction
+    )
+        public DutchExchange(
+          _TUL,
+          _OWL,
+          _owner,
+          _ETH,
+          _ETHUSDOracle,
+          _thresholdNewTokenPair,
+          _thresholdNewAuction
+        )
+    {}
 }
