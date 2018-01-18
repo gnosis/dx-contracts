e1eac9faed27cd2c91282de914bec79686bc3fd3
Velenir <Velenir@users.noreply.github.com>
2018-01-18 18:33:02 +0300

    added a test suite for settleFee function

--- test/dutchExchange-settleFee.js
+++ test/dutchExchange-settleFee.js
@@ -95,7 +95,7 @@ contract('DutchExchange - calculateFeeRatio', (accounts) => {
     assert.equal((num / den).toFixed(4), 0.0025, 'feeRatio is 0.25% when total TUL tokens > 0 but account\'s TUL balance == 1% total TUL')
   })
 
-  it('calculateFeeRatio works correctly when TokenTul.totalTokens() > 0 and account has 10 % total TUL', async () => {
+  it('calculateFeeRatio works correctly when TokenTul.totalTokens() > 0 and account has >= 10 % total TUL', async () => {
     const totalTul1 = await getTotalTUL()
     const lockedTULBalance1 = await getLockedTUL(seller1)
     const percent10 = Math.ceil((totalTul1 - lockedTULBalance1 / 0.1) / (1 / 0.1 - 1))
@@ -111,4 +111,58 @@ contract('DutchExchange - calculateFeeRatio', (accounts) => {
     const [num, den] = await calculateFeeRatio(seller1)
     assert.equal(num / den, 0, 'feeRatio is 0% when total TUL tokens > 0 but account\'s TUL balance == 10% total TUL')
   })
+})
+
+contract('DutchExchange - settleFee', (accounts) => {
+  const [master, seller1] = accounts
+  const testingAccs = accounts.slice(1, 5)
+
+  const ETHBalance = 10 ** 9
+
+  const GNOBalance = 10 ** 15
+
+  before(async () => {
+    // get contracts
+    contracts = await getContracts();
+    // destructure contracts into upper state
+    ({
+      // DutchExchange: dx,
+      EtherToken: eth,
+      TokenGNO: gno,
+      TokenTUL: tul,
+      // using internal contract with settleFeePub calling dx.settleFee internally
+      InternalTests: dx,
+    } = contracts)
+
+    // set up initial balances for accounts and allowance for dx in accounts' names
+    await Promise.all(testingAccs.map(acct => Promise.all([
+      eth.deposit({ from: acct, value: ETHBalance }),
+      eth.approve(dx.address, ETHBalance, { from: acct }),
+      gno.transfer(acct, GNOBalance, { from: master }),
+      gno.approve(dx.address, GNOBalance, { from: acct }),
+    ])))
+
+    await tul.updateMinter(master, { from: master })
+  })
+
+  after(eventWatcher.stopWatching)
+
+  /**
+   * Internally calls DutchExchange.settleFee
+   * @args Array of:
+   * @param {address} primaryToken
+   * @param {address} secondaryToken
+   * @param {uint} auctionIndex
+   * @param {address} user
+   * @param {uint} amount
+   */
+  const settleFee = (...args) => dx.settleFeePub(...args)
+
+  settleFee.call = async (...args) => (await dx.settleFeePub.call(...args)).toNumber()
+
+  const getTotalTUL = async () => (await tul.totalTokens.call()).toNumber()
+
+  const getLockedTUL = async account => (await tul.lockedTULBalances.call(account)).toNumber()
+
+  const mintTokens = (account, amount) => tul.mintTokens(account, amount, { from: master })
 })
