36e07b6c81e96a0cc677d4d65b8fb4a3c8f38e37
Velenir <Velenir@users.noreply.github.com>
2018-01-18 17:31:32 +0300

    abstracted away helper functons

--- test/dutchExchange-settleFee.js
+++ test/dutchExchange-settleFee.js
@@ -48,59 +48,67 @@ contract('DutchExchange - calculateFeeRatio', (accounts) => {
 
   after(eventWatcher.stopWatching)
 
+  const getTotalTUL = async () => (await tul.totalTokens.call()).toNumber()
+
+  const getLockedTUL = async account => (await tul.lockedTULBalances.call(account)).toNumber()
+
+  const mintTokens = (account, amount) => tul.mintTokens(account, amount, { from: master })
+
+  const calculateFeeRatio = async account => (await dx.calculateFeeRatioForJS.call(account)).map(n => n.toNumber())
+
   it('calculateFeeRatio works correctly when TokenTul.totalTokens() == 0', async () => {
-    const totalTul = (await tul.totalTokens.call()).toNumber()
+    const totalTul = await getTotalTUL()
 
     assert.strictEqual(totalTul, 0, 'initially no TUL tokens')
 
-    const [num, den] = (await dx.calculateFeeRatioForJS.call(seller1)).map(n => n.toNumber())
+    const [num, den] = await calculateFeeRatio(seller1)
     assert.strictEqual(num / den, 0.005, 'feeRatio is 0.5% when total TUL tokens == 0')
   })
 
   it('calculateFeeRatio works correctly when TokenTul.totalTokens() > 0', async () => {
     await tul.updateMinter(master, { from: master })
-    await tul.mintTokens(master, 1000, { from: master })
-    const totalTul = (await tul.totalTokens.call()).toNumber()
+    await mintTokens(master, 1000)
+    const totalTul = await getTotalTUL()
 
     assert.strictEqual(totalTul, 1000, 'there are available total TUL tokens')
 
-    const lockedTULBalance = (await tul.lockedTULBalances.call(seller1)).toNumber()
+    const lockedTULBalance = await getLockedTUL(seller1)
     assert.strictEqual(lockedTULBalance, 0, 'seller doesn\'t have TUL balance')
 
-    const [num, den] = (await dx.calculateFeeRatioForJS.call(seller1)).map(n => n.toNumber())
+    const [num, den] = await calculateFeeRatio(seller1)
     assert.strictEqual(num / den, 0.005, 'feeRatio is 0.5% when total TUL tokens > 0 but account\'s TUL balance == 0')
   })
 
   it('calculateFeeRatio works correctly when TokenTul.totalTokens() > 0 and account has 1 % total TUL', async () => {
-    const totalTul1 = (await tul.totalTokens.call()).toNumber()
+    const totalTul1 = await getTotalTUL()
     const percent1 = Math.round(totalTul1 / (1 / 0.01 - 1))
-    await tul.mintTokens(seller1, percent1, { from: master })
+    await mintTokens(seller1, percent1)
 
     assert.isAbove(totalTul1, 0, 'there are available total TUL tokens')
 
-    const totalTul2 = (await tul.totalTokens.call()).toNumber()
+    const totalTul2 = await getTotalTUL()
 
-    const lockedTULBalance = (await tul.lockedTULBalances.call(seller1)).toNumber()
+    const lockedTULBalance = await getLockedTUL(seller1)
     assert.strictEqual(lockedTULBalance, Math.round(totalTul2 * 0.01), 'seller has 1% of total TUL')
 
-    const [num, den] = (await dx.calculateFeeRatioForJS.call(seller1)).map(n => n.toNumber())
+    const [num, den] = await calculateFeeRatio(seller1)
     assert.equal((num / den).toFixed(4), 0.0025, 'feeRatio is 0.25% when total TUL tokens > 0 but account\'s TUL balance == 1% total TUL')
   })
 
   it('calculateFeeRatio works correctly when TokenTul.totalTokens() > 0 and account has 10 % total TUL', async () => {
-    const totalTul1 = (await tul.totalTokens.call()).toNumber()
-    const lockedTULBalance1 = (await tul.lockedTULBalances.call(seller1)).toNumber()
+    const totalTul1 = await getTotalTUL()
+    const lockedTULBalance1 = await getLockedTUL(seller1)
     const percent10 = Math.ceil((totalTul1 - lockedTULBalance1 / 0.1) / (1 / 0.1 - 1))
-    await tul.mintTokens(seller1, percent10, { from: master })
+    await mintTokens(seller1, percent10)
 
     assert.isAbove(totalTul1, 0, 'there are available total TUL tokens')
 
-    const totalTul2 = (await tul.totalTokens.call()).toNumber()
+    const totalTul2 = await getTotalTUL()
 
-    const lockedTULBalance2 = (await tul.lockedTULBalances.call(seller1)).toNumber()
+    const lockedTULBalance2 = await getLockedTUL(seller1)
     assert.strictEqual(lockedTULBalance2, Math.ceil(totalTul2 * 0.1), 'seller has 10% of total TUL')
 
-    const [num, den] = (await dx.calculateFeeRatioForJS.call(seller1)).map(n => n.toNumber())
+    const [num, den] = await calculateFeeRatio(seller1)
     assert.equal(num / den, 0, 'feeRatio is 0% when total TUL tokens > 0 but account\'s TUL balance == 10% total TUL')
   })
 })
