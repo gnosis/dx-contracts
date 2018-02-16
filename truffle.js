da91d2aa0b5359dafff77f73f7b6d90b1255ac65
josojo <josojo@hotmail.de>
2018-02-16 13:25:35 +0100

    not sure what I was doing

--- truffle.js
+++ truffle.js
@@ -1,22 +1,34 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs')
+
+const mnemonic = process.env.MNEMONIC
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // Match any network id
       gas: 6700000,
+      network_id: '*',
     },
     kovan: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: '42', // Match any network id
-      gas: 6700000,
+      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io'),
+      network_id: '42',
+      gas: 4500000,
+      gasPrice: 25000000000,
     },
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200,
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      network_id: '4',
+      gas: 4500000,
+      gasPrice: 25000000000,
     },
-  },
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
+      network_id: '0',
+      gas: 4500000,
+      gasPrice: 25000000000,
+    }
+  }
 }
