cf9b5f87402c5c9cf230582dfaffe903fd5c5517
josojo <josojo@hotmail.de>
2018-02-18 14:34:00 +0100

    deployment to kovan and rinkeby is possible, it will be deployed with infura

--- truffle.js
+++ truffle.js
@@ -13,22 +13,28 @@ module.exports = {
       network_id: '*',
     },
     kovan: {
-      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io'),
+      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io/'),
       network_id: '42',
-      gas: 4500000,
+      gas: 6700000,
       gasPrice: 25000000000,
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'),
       network_id: '4',
-      gas: 4500000,
+      gas: 6700000,
       gasPrice: 25000000000,
     },
     mainnet: {
       provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
       network_id: '0',
-      gas: 4500000,
+      gas: 6700000,
       gasPrice: 25000000000,
     }
-  }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
 }
