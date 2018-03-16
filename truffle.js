79ebe30e53c0a77c2d06e2e5da06453b7b4f667e
josojo <josojo@hotmail.de>
2018-03-16 14:11:25 +0100

    only change of truffle.js

--- truffle.js
+++ truffle.js
@@ -1,11 +1,35 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs')
+
+const mnemonic = process.env.MNEMONIC
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // Match any network id
       gas: 6700000,
+      network_id: '*',
     },
+    kovan: {
+      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io/'),
+      network_id: '42',
+      gas: 6700000,
+      gasPrice: 25000000000,
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'),
+      network_id: '4',
+      gas: 6700000,
+      gasPrice: 25000000000,
+    },
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
+      network_id: '0',
+      gas: 6700000,
+      gasPrice: 25000000000,
+    }
   },
   solc: {
     optimizer: {
