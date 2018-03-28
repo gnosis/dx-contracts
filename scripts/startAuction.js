05ded659f7793c1f20af81755511bb4e564087ea
David Sato <david.sato64@gmail.com>
2018-03-28 13:13:02 +0200

    added minimist argv flag support for network + tokenToAdd + cleaned up async logic

--- scripts/startAuction.js
+++ scripts/startAuction.js
@@ -1,8 +1,9 @@
+// @ts-check
 // script crashes for gas reason sometimes, if it is run against testrpc. with kovan or rinkeby, there were no porblems dectected.
 
 const Web3 = require('web3')
 const fs = require('fs')
-
+const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
 
 //optional for private keys
 /*
@@ -11,12 +12,14 @@ const privKey = "process.env.PrivateKEY // raw private key
 const provider = new HDWalletProvider(privKey, "https://kovan.infura.io/");
 */
 
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const mnemonic = process.env.MNEMONIC
-const provider = new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/')
-//const provider = new HDWalletProvider(mnemonic, 'http://localhost:8545')
-// important: gas needs to specified for ganache with local test net for whatever reason.
-web3 = new Web3(provider.engine)
+let web3
+if (argv.network) {
+  const HDWalletProvider = require('truffle-hdwallet-provider')
+  const provider = new HDWalletProvider(process.env.MNEMONIC, 'https://rinkeby.infura.io/')
+  web3 = new Web3(provider.engine)
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
 const TruffleContract = require('truffle-contract')
 
 // retrieve truffle-contracts 
@@ -37,59 +40,62 @@ Proxy.setProvider(web3.currentProvider)
 TokenOMG.setProvider(web3.currentProvider)
 TokenRDN.setProvider(web3.currentProvider)
 
-// Test VARS
-let eth
-let gno
-let rdn
-let dx
-let Token
+module.exports = (async () => {
+  const promisedAcct = new Promise((a, r) => web3.eth.getAccounts((e, r) => a(r[0])))
 
-const startingETH = 18e18
-const startingToken = 100e18
+  // Test VARS
+  let eth
+  let gno
+  let rdn
+  let dx
+  let Token
 
-const getContracts = async () => {
-  eth = await EtherToken.deployed()
-  omg = await TokenOMG.deployed()
-  rdn = await TokenRDN.deployed()
-  const proxy = await Proxy.deployed()
-  dx = DutchExchange.at(proxy.address)
-  return {
-    EtherToken: eth,
-    TokenOMG: omg,
-    TokenRDN: rdn,
-    DutchExchange: dx,
+  const tokenMap = {
+    eth,
+    gno,
+    rdn,
+    Token
   }
-}
-const setup = async () => {
-  await eth.deposit({ from: acct, value: startingETH })
-  await eth.approve(dx.address, startingETH, { from: acct })
-  await tokenToAdd.approve(dx.address, startingToken+20, { from: acct })
-  await dx.deposit(tokenToAdd.address, startingToken, { from: acct, gas: 234254})
-  await dx.deposit(eth.address, startingETH, { from: acct, gas: 234254})
-}
 
-const p = new Promise((resolve, reject) => {
-  web3.eth.getAccounts((error, result) => {
-    resolve(result)
-  })
-})
+  const startingETH = 18e18
+  const startingToken = 100e18
 
-// parameters for deployment
-let acct
-let tokenToAdd
-p.then((a) => {
-  acct = a[0]
-  return getContracts()
-})
-  .then((c) => {
-    eth = c.EtherToken
-    tokenToAdd = c.TokenRDN
-    dx = c.DutchExchange
-    return setup()})
-  .then(() => dx.balances(eth.address, acct))
-  .then((t) => {
-    return dx.getAuctionIndex(eth.address, tokenToAdd.address)
-  })
-  .then((t) => {
-    return dx.addTokenPair(eth.address, tokenToAdd.address, startingETH, 0, 1, 50, { from: acct, gas: 2374235})
-  })
+  const getContracts = async () => {
+    const proxy = await Proxy.deployed()
+    eth = await EtherToken.deployed()
+    omg = await TokenOMG.deployed()
+    rdn = await TokenRDN.deployed()
+    dx = DutchExchange.at(proxy.address)
+    Token = argv.token && tokenMap[argv.token] ? tokenMap[argv.token] : rdn
+
+    return {
+      EtherToken: eth,
+      TokenOMG: omg,
+      TokenRDN: rdn,
+      DutchExchange: dx,
+    }
+  }
+  const setup = async (a, tta) => {
+    await eth.deposit({ from: a, value: startingETH })
+    await eth.approve(dx.address, startingETH, { from: a })
+    await tta.approve(dx.address, startingToken+20, { from: a })
+    await dx.deposit(tta.address, startingToken, { from: a, gas: 234254})
+    await dx.deposit(eth.address, startingETH, { from: a, gas: 234254})
+  }
+
+  try {
+    const acct = await promisedAcct
+    await getContracts()
+    await setup(acct, Token)
+
+    const receipt = await dx.addTokenPair(eth.address, Token.address, startingETH, 0, 1, 50, { from: acct, gas: 2374235})
+    console.log(`
+    ===========================
+    Successfully added ${await Token.name.call()}
+    Auction Index => ${(await dx.getAuctionIndex.call(eth.address, Token.address)).toNumber()}
+    ===========================
+    `)
+  } catch (error) {
+    throw new Error(error)
+  }
+})()
