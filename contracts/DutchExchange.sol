a5075c682c1361ecf4441295b8637f57ff986343
josojo <josojo@hotmail.de>
2018-04-04 20:59:29 +0200

    added claimBuyerFunds

--- contracts/DutchExchange.sol
+++ contracts/DutchExchange.sol
@@ -1332,7 +1332,7 @@ contract DutchExchange {
     //@param auctionBuyTokens are the buyTokens defining an auctionPair
     //@param auctionIndices are the auction indices on which an token should be claimedAmounts
     //@param user is the user who wants to his tokens
-    function claimTokensFromSeveralAuctions(
+    function claimTokensFromSeveralAuctionsAsSeller(
         address[] auctionSellTokens,
         address[] auctionBuyTokens,
         uint[] auctionIndices,
@@ -1350,7 +1350,29 @@ contract DutchExchange {
         for (uint i = 0; i < length; i++)
             claimSellerFunds(auctionSellTokens[i], auctionBuyTokens[i], user, auctionIndices[i]);
     }
+    //@dev for multiple withdraws
+    //@param auctionSellTokens are the sellTokens defining an auctionPair
+    //@param auctionBuyTokens are the buyTokens defining an auctionPair
+    //@param auctionIndices are the auction indices on which an token should be claimedAmounts
+    //@param user is the user who wants to his tokens
+    function claimTokensFromSeveralAuctionsAsBuyer(
+        address[] auctionSellTokens,
+        address[] auctionBuyTokens,
+        uint[] auctionIndices,
+        address user
+    )
+        external
+    {
+        uint length = auctionSellTokens.length;
+        uint length2 = auctionBuyTokens.length;
+        require(length == length2);
 
+        uint length3 = auctionIndices.length;
+        require(length2 == length3);
+
+        for (uint i = 0; i < length; i++)
+            claimBuyerFunds(auctionSellTokens[i], auctionBuyTokens[i], user, auctionIndices[i]);
+    }
     // > Events
     event NewDeposit(
          address token,
