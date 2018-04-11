5047e5506c18d007aa21c1d0a9cf8a8cd774b297
josojo <josojo@hotmail.de>
2018-04-11 16:54:51 +0200

    finalized and tested script

--- scripts/startAuction.js
+++ scripts/startAuction.js
@@ -3,38 +3,39 @@
  * to add a new TradingPair ETH:Token to the DutchExchange
  * @flags:
  * --network                    if not specified, testrpc will be used. Otherwise rinkeby             
- * --tokenToAddAddress              
- * --priceNum                   price is given in units [EtherToken]/[buyToken]
- * --priceDen                   
- * --tokenNR                    selecting the Token from the list tokenMap
+ * --tokenToAddAddress          any token that inherits the StandartToken functions can be submitted    
+ * --priceNum                   price is given in units [EtherToken]/[tokenToAdd]
+ * --priceDen
+ * --fundingETH                 how much Ether should be sold in the first auction
+ * --fundingToken               how much Tokens should be deposited on the Exchange    
  */
 
 const Web3 = require('web3')
 const fs = require('fs')
-const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
+const argv = require('minimist')(process.argv.slice(2), { string: 'a', string: 'tokenToAddAddress' })
 
-//optional for private keys
-/*
+//optional for MNEMONICs
+//const mnemonic = process.env.MNEMONIC // Mnemonic for account
+//const HDWalletProvider = require('truffle-hdwallet-provider')
+//const provider = new HDWalletProvider(process.env.MNEMONIC, 'https://rinkeby.infura.io/')
+
+
+const privKey = process.env.PrivateKEY // raw private key
 const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
-const privKey = "process.env.PrivateKEY // raw private key
-const provider = new HDWalletProvider(privKey, "https://kovan.infura.io/");
-*/
 
 let web3
 if (argv.network) {
-  const HDWalletProvider = require('truffle-hdwallet-provider')
-  const provider = new HDWalletProvider(process.env.MNEMONIC, 'https://rinkeby.infura.io/')
+  const provider = new HDWalletProvider(privKey, 'https://rinkeby.infura.io/')
   web3 = new Web3(provider.engine)
 } else {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+
 const TruffleContract = require('truffle-contract')
 
 // retrieve truffle-contracts 
 const EtherTokenJson = JSON.parse(fs.readFileSync('./build/contracts/EtherToken.json'))
 const EtherToken = TruffleContract(EtherTokenJson)
-const TokenOMGJson = JSON.parse(fs.readFileSync('./build/contracts/TokenOMG.json'))
-const TokenOMG = TruffleContract(TokenOMGJson)
 const TokenRDNJson = JSON.parse(fs.readFileSync('./build/contracts/TokenRDN.json'))
 const TokenRDN = TruffleContract(TokenRDNJson)
 const StandardTokenJson = JSON.parse(fs.readFileSync('./build/contracts/StandardToken.json'))
@@ -43,68 +44,77 @@ const ProxyJson = JSON.parse(fs.readFileSync('./build/contracts/Proxy.json'))
 const Proxy = TruffleContract(ProxyJson)
 const DutchExchangeJson = JSON.parse(fs.readFileSync('./build/contracts/DutchExchange.json'))
 const DutchExchange = TruffleContract(DutchExchangeJson)
+const PriceOracleInterfaceJson = JSON.parse(fs.readFileSync('./build/contracts/PriceOracleInterface.json'))
+const PriceOracleInterface = TruffleContract(PriceOracleInterfaceJson)
 
+//linking provider
 EtherToken.setProvider(web3.currentProvider)
 DutchExchange.setProvider(web3.currentProvider)
 Proxy.setProvider(web3.currentProvider)
-TokenOMG.setProvider(web3.currentProvider)
 TokenRDN.setProvider(web3.currentProvider)
+StandardToken.setProvider(web3.currentProvider)
+PriceOracleInterface.setProvider(web3.currentProvider)
 
 module.exports = (async () => {
   const promisedAcct = new Promise((a, r) => web3.eth.getAccounts((e, r) => a(r[0])))
 
   // Test VARS
   let eth
-  let omg
   let rdn
   let dx
   let Token
 
-  const tokenMap = {
-    eth,
-    omg,
-    rdn,
-    Token
-  }
-
-  const startingETH = argv.fundingETH ? argv.fundingETH : 18e18
-  const startingToken = argv.fundingToken ? argv.fundingToken : 100e18
+  const sellVolumeInETH = argv.fundingETH ? argv.fundingETH : 18e18
+  const startingToken = argv.fundingToken ? argv.fundingToken : 0
 
   const getContracts = async () => {
     const proxy = await Proxy.deployed()
     eth = await EtherToken.deployed()
-    omg = await TokenOMG.deployed()
     rdn = await TokenRDN.deployed()
     dx = DutchExchange.at(proxy.address)
-    Token = argv.tokenNR && tokenMap[argv.tokenNR] ? tokenMap[argv.tokenNR] : rdn
     if(argv.tokenToAddAddress){
       Token = StandardToken.at(argv.tokenToAddAddress)
+    } else{
+      Token = rdn
     }
     return {
       EtherToken: eth,
-      TokenOMG: omg,
       TokenRDN: rdn,
       DutchExchange: dx,
     }
   }
   const setup = async (a, tta) => {
-    await eth.deposit({ from: a, value: startingETH })
-    await eth.approve(dx.address, startingETH, { from: a })
+
+    if( (await web3.eth.getBalance(a)).toNumber()< sellVolumeInETH)
+      throw("Not enough Eth funds availbale")
+
+    await eth.deposit({ from: a, value: sellVolumeInETH })
+    await eth.approve(dx.address, sellVolumeInETH, { from: a })
     await tta.approve(dx.address, startingToken, { from: a })
+
+    if( (await tta.balanceOf(a)).toNumber()< startingToken)
+      throw("Not enough funds of the buyTokens are availbale")
+
     await dx.deposit(tta.address, startingToken, { from: a, gas: 234254})
-    return dx.deposit(eth.address, startingETH, { from: a, gas: 234254})
+    return dx.deposit(eth.address, sellVolumeInETH, { from: a, gas: 234254})
+  }
+  const checkETHFundingSufficient = async (a, tta) => {
+    const oracle = await PriceOracleInterface.deployed()
+    const price = (await oracle.getUSDETHPrice.call()).toNumber()
+    if(price*sellVolumeInETH/1e18 < 10000)
+      throw("ETHFunding not sufficient")
+    return;
   }
 
   try {
     const acct = await promisedAcct
     await getContracts()
-
+    await checkETHFundingSufficient()
     await setup(acct, Token)
-
-    const receipt = await dx.addTokenPair(eth.address, Token.address, startingETH, 0, argv.priceNum ? argv.priceNum : 1, argv.priceDen ? argv.priceDen : 1, { from: acct, gas: 2374235})
+    const receipt = await dx.addTokenPair(eth.address, Token.address, sellVolumeInETH, 0, argv.priceNum ? argv.priceNum : 1, argv.priceDen ? argv.priceDen : 1, { from: acct, gas: 2374235})
     console.log(`
     ===========================
-    Successfully added  => [Ether Token // ${await Token.name.call()}] Auction
+    Successfully added  => [Ether Token // ${await Token.address}] Auction
     Auction Index       => ${(await dx.getAuctionIndex.call(eth.address, Token.address)).toNumber()}
     Receipt.tx          => ${JSON.stringify(receipt.tx, false, 2)}
     ===========================
