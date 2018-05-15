589250276fae24f30a16434fe3c242d21a68d0eb
josojo <josojo@hotmail.de>
2018-05-15 08:22:54 +0200

    new getApprovalTokens

--- contracts/DutchExchange.sol
+++ contracts/DutchExchange.sol
@@ -1430,6 +1430,25 @@ contract DutchExchange {
         return buyersBalances;
     }
 
+    //@dev for quick overview of approved Tokens
+    //@param addressesToCheck are the ERC-20 token addresses to be checked whether they are approved
+    function getApprovedAddressesOfList(
+        address[] addressToCheck
+    )
+        external
+        view
+        returns (bool[])
+    {
+        uint length = addressToCheck.length;
+
+        bool[] memory isApproved = new bool[](length);
+
+        for (uint i = 0; i < length; i++) {
+            isApproved[i] = approvedTokens[addressToCheck[i]];
+        }
+
+        return isApproved;
+    }
 
     //@dev for multiple withdraws
     //@param auctionSellTokens are the sellTokens defining an auctionPair
