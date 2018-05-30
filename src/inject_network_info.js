f47fce9ca4e491420f15a34fbd15299293913688
Angel Rodriguez <anxolin@gmail.com>
2018-05-30 19:22:10 +0200

    Add extract/inject scripts

---
+++ src/inject_network_info.js
@@ -0,0 +1,8 @@
+const injectNetworks = require('@gnosis.pm/util-contracts/src/util/injectNetworks')
+const path = require('path')
+
+const DEFAULT_CONF_FILE = path.join(__dirname, './conf/network-restore')
+
+const confFile = process.env.CONF_FILE || DEFAULT_CONF_FILE
+injectNetworks(confFile)
+  .catch(console.error)
