256fb74c4e6b052b566667fd48e0a682794fca67
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-06-01 17:17:44 +0200

    Add automatic testing in Travis for 'master' and 'develop'

--- travscripts/BranchScript.sh
+++ travscripts/BranchScript.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH =~ (feature/test-?(\/[a-zA-Z0-9/._-]*)?) ]]; then
-  echo " ==> Detected a CONTRACT(S) branch"
+if [[ $TRAVIS_BRANCH = "master" || $TRAVIS_BRANCH = "develop" ]]; then
+  echo " ==> Detected PRINCIPAL branch - compiling and testing contracts"
   #jump back to root
   cd $TRAVIS_BUILD_DIR
   echo " ==> JUMPING LOCATIONS: NOW IN $TRAVIS_BUILD_DIR"
-  #run solcover
+
+  # running contracts tests
   echo " ==> RUNNING test"
-
   npm test;
-  # echo " ==> RUNNING solidity-coverage" &&
-  # ./node_modules/.bin/solidity-coverage
-  # #run codecov
-  # echo " ==> RUNNING codecov" &&
-  # ./node_modules/.bin/codecov
-
-  #&& cat coverage/lcov.info | coveralls;
 else
-  echo " ==> Detected a NON-CONTRACT(S) branch - compiling contracts and testing App compile"
-  cd $TRAVIS_BUILD_DIR
-
-  if [[ $TRAVIS_BRANCH = "master" ]]; then
-    echo " ==> MASTER branch detected, running PRODUCTION BUILD"
-
-    truffle compile --all && npm run build:prod;
-  else
-    echo " ==> NON-MASTER branch detected, running DEVELOPMENT BUILD"
-
-    truffle compile --all && npm run build:dev;
-  fi;
+  echo " ==> No execution for branches other than MASTER or DEVELOP"
 fi;
-
-#RUBY /contrac[ts?\/\S?]+/i
-#JS /contracts?(\/[a-zA-Z0-9/._-]*)?/giu
