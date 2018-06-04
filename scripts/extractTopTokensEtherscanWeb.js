1ec489cf6f01919944ac6125d0eea17e8d26db1a
Angel Rodriguez <anxolin@gmail.com>
2018-06-04 18:35:53 +0200

    Update top 150 tokens from Etherscan

--- scripts/extractTopTokensEtherscanWeb.js
+++ scripts/extractTopTokensEtherscanWeb.js
@@ -3,27 +3,35 @@ function toArray (items) {
   return [].slice.call(items)   
  }
  
- var rowsHtml = document.querySelectorAll('#ContentPlaceHolder1_divresult tr')
- var rows = toArray(rowsHtml).slice(1)
- var tokens = rows
-   .map(row => row.querySelector(':nth-child(3) h5 a'))
-   .map(link => {
-     var nameAndSymbolRegex = /([\w\s]+) \((\w+)\)/gi
-     var addressRegex = /http:\/\/etherscan.io\/token\/0x(\w+)/gi
-     let nameAndSymbolMatch = nameAndSymbolRegex.exec(link.innerText)
-     let addressMatch = addressRegex.exec(link.href)
- 
-     return {
-       name: nameAndSymbolMatch[1],
-       symbol: nameAndSymbolMatch[2],
-       address: addressMatch[1],
-       approve: true,
-       etherScanLink: link.href
-     }
-   })
- 
- 
- 
- console.log(tokens)
- console.log(JSON.stringify(tokens), null, 2)
- 
+var rowsHtml = document.querySelectorAll('#ContentPlaceHolder1_divresult tr')
+var rows = toArray(rowsHtml).slice(1)
+var tokens = rows
+  .map(row => row.querySelector(':nth-child(3) h5 a'))
+  .map(link => {
+    var nameAndSymbolRegex = /([\w\s]+) \((\w+)\)/gi
+    var addressRegex = /http:\/\/etherscan.io\/token\/0x(\w+)/gi
+    let nameAndSymbolMatch = nameAndSymbolRegex.exec(link.innerText)
+    let addressMatch = addressRegex.exec(link.href)
+
+    let name, symbol, address
+    if (nameAndSymbolMatch) {
+      name = nameAndSymbolMatch[1]
+      symbol = nameAndSymbolMatch[2]
+    }
+    if (addressMatch) {
+      address = addressMatch[1]
+    }
+
+    if (nameAndSymbolMatch)
+
+    return {
+      name,
+      symbol,
+      address,
+      approve: true,
+      etherScanLink: link.href
+    }
+  })
+
+console.log(tokens)
+console.log(JSON.stringify(tokens, null, 2))
