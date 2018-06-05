d1fba8d2009996f56ecfe046c35f118730ed3a9a
Angel Rodriguez <anxolin@gmail.com>
2018-06-05 11:20:25 +0200

    Add the feed expire period

--- migrations/3_DEV-deploy_price_feed.js
+++ migrations/3_DEV-deploy_price_feed.js
@@ -2,8 +2,8 @@
 /* eslint no-undef: "error" */
 const contract = require('truffle-contract')
 
-const ETH_USD_PRICE = 500
-const MAGIC_NUMBER_TO_BE_RENAMED = 1516168838 * 2
+const ETH_USD_PRICE = process.env.ETH_USD_PRICE || 500 // 500 USD/ETH 
+const FEED_EXPIRE_PERIOD_DAYS = process.env.FEED_EXPIRE_PERIOD_DAYS || 365 // 1 year
 
 const PriceFeed = artifacts.require('PriceFeed')
 const Medianizer = artifacts.require('Medianizer')
@@ -19,6 +19,17 @@ function getMedianizerAddress () {
   }  
 }
 
+function getTime () {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBlock('latest', (err, block) => {
+      if (err) {
+        return reject(err)
+      } else {
+        resolve(block.timestamp)
+      }
+    })
+  })
+}
 
 module.exports = function (deployer, network, accounts) {
   const medianizerAddress = getMedianizerAddress()
@@ -30,10 +41,13 @@ module.exports = function (deployer, network, accounts) {
       .then(() => deployer.deploy(PriceOracleInterface, account, Medianizer.address))
       .then(() => Medianizer.deployed())
       .then(medianizer => medianizer.set(PriceFeed.address, { from: account }))
-      .then(() => PriceFeed.deployed())
-      .then(priceFeed => priceFeed.post(
+      .then(() => Promise.all([
+        PriceFeed.deployed(),
+        getTime()
+      ])
+      .then(([ priceFeed, time ]) => priceFeed.post(
         ETH_USD_PRICE * 1e18,
-        MAGIC_NUMBER_TO_BE_RENAMED,
+        now + FEED_EXPIRE_PERIOD_DAYS * 24 * 60 * 60,
         Medianizer.address, {
           from: account
         })
