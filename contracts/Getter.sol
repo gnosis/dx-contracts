22e12b6e59a60866ff5682be2c0f43a80bfa1244
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-06-15 17:58:35 +0200

    Add contracts needed for trufflescripts and deploy

--- contracts/Getter.sol
+++ contracts/Getter.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.4.21;
- 
+
+/*
+  External dependencies contracts
+  As on truffle version 4.1.5, is not posible to run migrations from dependencies,
+  that use contracts which aren`t explicitly imported in the base project
+*/
 import "@gnosis.pm/util-contracts/contracts/StandardToken.sol";
 import "@gnosis.pm/util-contracts/contracts/EtherToken.sol";
 import "@gnosis.pm/gno-token/contracts/TokenGNO.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWLProxy.sol";
 import "@gnosis.pm/owl-token/contracts/OWLAirdrop.sol";
+
+/* FIXME only needed for testing and deploy purposes */
+import "test/contracts/TokenRDN.sol";
+import "test/contracts/TokenOMG.sol";
 
 
 contract Getter {
