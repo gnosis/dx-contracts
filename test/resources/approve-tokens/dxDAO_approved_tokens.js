ac7292f02bf576974515b0a3d9d48195a8230002
David <david.sato64@gmail.com>
2019-01-02 15:26:02 +0100

    generated new tokenApproved JS file

---
+++ test/resources/approve-tokens/dxDAO_approved_tokens.js
@@ -0,0 +1,163 @@
+module.exports = [
+    {
+        "name": "Basic Attention Token",
+        "symbol": "BAT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+        "address": "0x0d8775f648430679a709e98d2b0cb6250d2887ef"
+    },
+    {
+        "name": "Bancor",
+        "symbol": "BNT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c",
+        "address": "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c"
+    },
+    {
+        "name": "Civic",
+        "symbol": "CVC",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x41e5560054824ea6b0732e656e3ad64e20e94e45",
+        "address": "0x41e5560054824ea6b0732e656e3ad64e20e94e45"
+    },
+    {
+        "name": "Dai",
+        "symbol": "DAI",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+        "address": "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
+    },
+    {
+        "name": "district0x",
+        "symbol": "DNT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x0abdace70d3790235af448c88547603b945604ea",
+        "address": "0x0abdace70d3790235af448c88547603b945604ea"
+    },
+    {
+        "name": "Decentraland",
+        "symbol": "MANA",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
+        "address": "0x0f5d2fb29fb7d3cfee444a200298f468908cc942"
+    },
+    {
+        "name": "Foam",
+        "symbol": "FOAM",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x4946fcea7c692606e8908002e55a582af44ac121",
+        "address": "0x4946fcea7c692606e8908002e55a582af44ac121"
+    },
+    {
+        "name": "Gnosis",
+        "symbol": "GNO",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x6810e776880c02933d47db1b9fc05908e5386b96",
+        "address": "0x6810e776880c02933d47db1b9fc05908e5386b96"
+    },
+    {
+        "name": "Golem",
+        "symbol": "GNT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xa74476443119A942dE498590Fe1f2454d7D4aC0d",
+        "address": "0xa74476443119A942dE498590Fe1f2454d7D4aC0d"
+    },
+    {
+        "name": "KyberNetwork",
+        "symbol": "KNC",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xdd974d5c2e2928dea5f71b9825b8b646686bd200",
+        "address": "0xdd974d5c2e2928dea5f71b9825b8b646686bd200"
+    },
+    {
+        "name": "Livepeer",
+        "symbol": "LPT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x58b6a8a3302369daec383334672404ee733ab239",
+        "address": "0x58b6a8a3302369daec383334672404ee733ab239"
+    },
+    {
+        "name": "Loom",
+        "symbol": "LOOM",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xa4e8c3ec456107ea67d3075bf9e3df3a75823db0",
+        "address": "0xa4e8c3ec456107ea67d3075bf9e3df3a75823db0"
+    },
+    {
+        "name": "Maker",
+        "symbol": "MKR",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+        "address": "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"
+    },
+    {
+        "name": "Melonport",
+        "symbol": "MLN",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xBEB9eF514a379B997e0798FDcC901Ee474B6D9A1",
+        "address": "0xBEB9eF514a379B997e0798FDcC901Ee474B6D9A1"
+    },
+    {
+        "name": "Numeraire",
+        "symbol": "NMR",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x1776e1f26f98b1a5df9cd347953a26dd3cb46671",
+        "address": "0x1776e1f26f98b1a5df9cd347953a26dd3cb46671"
+    },
+    {
+        "name": "OmiseGO",
+        "symbol": "OMG",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
+        "address": "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"
+    },
+    {
+        "name": "Polymath",
+        "symbol": "POLY",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x9992ec3cf6a55b00978cddf2b27bc6882d88d1ec",
+        "address": "0x9992ec3cf6a55b00978cddf2b27bc6882d88d1ec"
+    },
+    {
+        "name": "REP",
+        "symbol": "REP",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xe94327d07fc17907b4db788e5adf2ed424addff6",
+        "address": "0xe94327d07fc17907b4db788e5adf2ed424addff6"
+    },
+    {
+        "name": "StatusNetwork",
+        "symbol": "SNT",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x744d70fdbe2ba4cf95131626614a1763df805b9e",
+        "address": "0x744d70fdbe2ba4cf95131626614a1763df805b9e"
+    },
+    {
+        "name": "Storj",
+        "symbol": "STORJ",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac",
+        "address": "0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac"
+    },
+    {
+        "name": "USD Coin",
+        "symbol": "USDC",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    },
+    {
+        "name": "Zilliqa",
+        "symbol": "ZIL",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0x05f4a42e251f2d52b8ed15e9fedaacfcef1fad27",
+        "address": "0x05f4a42e251f2d52b8ed15e9fedaacfcef1fad27"
+    },
+    {
+        "name": "ZRX",
+        "symbol": "ZRX",
+        "approve": true,
+        "etherScanLink": "https://etherscan.io/token/0xe41d2489571d322189246dafa5ebde1f4699f498",
+        "address": "0xe41d2489571d322189246dafa5ebde1f4699f498"
+    }
+]
