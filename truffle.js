ac71e45a3d6b24e3c5f98a6cebe8650a0a9ec2b3
Angel Rodriguez <anxolin@gmail.com>
2019-01-11 20:00:22 +0100

    Fix gas issue with the tests

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 const truffleConfig = require('@gnosis.pm/util-contracts/src/util/truffleConfig')
 
 const DEFAULT_GAS_PRICE_GWEI = 5
-const DEFAULT_GAS_LIMIT = 6.5e6
+const DEFAULT_GAS_LIMIT = 6721975
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
 // Load env vars
