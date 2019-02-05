a6fa4611bdeb568df08642b69ae1fcff3b8d42b6
Angel Rodriguez <anxolin@gmail.com>
2019-02-05 14:06:02 +0100

    Test approve for RDN and OMG

---
+++ test/resources/approve-tokens/rdnAndOmg-rinkeby.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    'name': 'Wrapped Ether',
+    'symbol': 'WETH',
+    'approve': true,
+    'etherScanLink': 'https://etherscan.io/token/0xc778417e063141139fce010982780140aa0cd5ab',
+    'address': '0xc778417e063141139fce010982780140aa0cd5ab'
+  },
+  {
+    'name': 'OmiseGO',
+    'symbol': 'OMG',
+    'approve': true,
+    'etherScanLink': 'https://etherscan.io/token/0x00df91984582e6e96288307e9c2f20b38c8fece9',
+    'address': '0x00df91984582e6e96288307e9c2f20b38c8fece9'
+  },
+  {
+    'name': 'Raiden',
+    'symbol': 'RDN',
+    'approve': true,
+    'etherScanLink': 'https://etherscan.io/token/0x3615757011112560521536258c1e7325ae3b48ae',
+    'address': '0x3615757011112560521536258c1e7325ae3b48ae'
+  }
+]
